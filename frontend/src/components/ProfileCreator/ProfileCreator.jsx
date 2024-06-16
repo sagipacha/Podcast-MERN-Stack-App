dd448a7a85cf2b37e9d85b9ba739cebb926583d0
sagi pacharkovsky <139847862+sagipacha@users.noreply.github.com>
2024-06-16 17:12:33 +0300

    Add global colors

--- frontend/src/components/ProfileCreator/ProfileCreator.jsx
+++ frontend/src/components/ProfileCreator/ProfileCreator.jsx
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { UserContext } from "../../../Context/User";
+import { UserContext } from "../../Context/User";
 import { Link } from "react-router-dom";
 import styles from "./ProfileCreator.module.css";
 
